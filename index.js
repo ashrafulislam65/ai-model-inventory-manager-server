3ad9a9b98ada59588f43bf7c9c1afff032e735cd
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2025-11-13 21:26:21 +0600

    env variable added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://AIMDB:ULYqXbKUWy1kCGLO@cluster0.fcejyck.mongodb.net/?appName=Cluster0";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fcejyck.mongodb.net/?appName=Cluster0`;
 const app = express();
 const port = process.env.PORT || 3000;
+
 // Middleware
 app.use(cors());
 app.use(express.json());
